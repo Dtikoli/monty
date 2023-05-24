68a5b68f1768a9cd142c80aa98d7035a3a7d516e
Dtikoli <tikolidavid@yahoo.com>
2023-05-24 08:58:16 -0700

    Adding tokenizer

---
+++ tokenizer.c
@@ -0,0 +1,72 @@
+#include "monty.h"
+
+/**
+ * words_counter - count the number of words in a from a line
+ * @s: string to evaluate
+ * Return: number of words
+ */
+int words_counter(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings
+ */
+char **strtow(char *str)
+{
+	char **ptr, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	len = strlen(str);
+	words = words_counter(str);
+	if (words == 0)
+		return (NULL);
+
+	ptr = (char **) malloc(sizeof(char *) * (words + 1));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (isspace(str[i]) || str[i] == '\0' || str[i] == '\n')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				ptr[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	ptr[k] = NULL;
+
+	return (ptr);
+}
