24fad65f22a6d3064123c41fd94d4fdb0ef1c5de
Dtikoli <tikolidavid@yahoo.com>
2023-05-25 07:21:00 -0700

    Your monty interpretor is ready!

--- opecode_func4.c
+++ opecode_func4.c
@@ -34,20 +34,20 @@ void _pchar(stack_t **stack, unsigned int nline)
  */
 void _pstr(stack_t **stack, unsigned int nline)
 {
-	stack_t *cnode = *stack;
+	stack_t *current = *stack;
 
 	(void)nline;
 
-	if (!cnode)
+	if (!current)
 	{
 		putchar('\n');
 		return;
 	}
 
-	while (cnode && cnode->n != 0 && cnode->n >= 0 && cnode->n <= 127)
+	while (current && current->n > 0 && current->n <= 127)
 	{
-		putchar(cnode->n);
-		cnode = cnode->next;
+		putchar(current->n);
+		current = current->next;
 	}
 
 	putchar('\n');
